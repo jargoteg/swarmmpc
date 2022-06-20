67d03fe39c20bf09d3fa32ef5201da68aedc8275
Jahir-PC <jahirargote@gmail.com>
2022-06-20 23:25:09 +0100

    python code update using mpc and mhe implementation ... as example

--- simple_MPC.py
+++ simple_MPC.py
@@ -17,7 +17,6 @@ x_min = -10    #Size of the arena
 y_max = 1
 y_min = -1
 #limits = [x_min x_max ; y_min y_max]
-
 v_max = 0.6
 v_min = -v_max
 omega_max = pi/4
@@ -32,6 +31,21 @@ states = []
 inputs = []
 rhs = []
 
+def shift_timestep(sampling_time, t0,x0,u,f):
+    f_value = f(x0, u[:,0])
+    next_state = DM.full(x0 + (sampling_time * f_value))
+
+    t0 = t0 + sampling_time
+    u0 = horzcat(
+            u[:, 1:],
+            reshape(u[:, -1], -1, 1)
+    )
+
+    return t0, next_state, u0
+
+def DM2Arr(dm):
+    return np.array(dm.full())
+
 
 
 for p in range(0,n_robots): #define states, inputs and dynamics for each robot
@@ -39,24 +53,32 @@ for p in range(0,n_robots): #define states, inputs and dynamics for each robot
     y.append(SX.sym('y'+str(p)))
     theta.append(SX.sym('theta'+str(p)))
     states.extend([x[p],y[p],theta[p]])
-
+    print(states)
     v.append(SX.sym('v'+str(p)))
     omega.append(SX.sym('omega'+str(p)))
     inputs.extend([v[p],omega[p]])
 
     rhs.extend([v[p]*cos(theta[p]),v[p]*sin(theta[p]),omega[p]])  #system r.h.s (right hand side of system dynamics)
 
+
+print(x)
+print(y)
+
+#x = SX.sym('x',5)
+print(x)
+
+
 n_states = len(states)
 n_inputs = len(inputs) #Get number of states and inputs
 
-f = Function('f',states+inputs,rhs) #nonlinear mapping function f(x,u)
+f = Function('f',[states, inputs],[rhs]) #nonlinear mapping function f(x,u)
 U = SX.sym('U',n_inputs,N) #Decision variables (inputs)
 P = SX.sym('P',n_states + n_states) #parameters (which include at the initial state of the robot and the reference state)
 
 X = SX.sym('X',n_states,(N+1)) #A vector that represents the states over the optimization problem.
 
-obj = 0; #Objective function
-g = [];  #Constraints vector
+obj = 0 #Objective function
+g = []  #Constraints vector ~~check
 
 Q = np.zeros((n_states,n_states)) #weighting matrices (states)
 R = np.zeros((n_inputs,n_inputs)) #weighting matrices (inputs)
@@ -68,8 +90,6 @@ for nj in range(0,n_robots): #%Populate Q and R matrices for all robots (for 3 s
     R[2*(nj),2*(nj)] = 0.5
     R[1+2*(nj),1+2*(nj)] = 0.05 
 
-print(Q)
-print(R)
 
 st  = X[:,0] #initial state
 g.append(st-P[0:n_robots*3]) #initial condition constraints for initial state (3 is the amount of states per robot)
@@ -78,8 +98,16 @@ for k in range(0,N):
     st = X[:,k]
     inp = U[:,k]; #st=state; inp=input
     st_error = (st-P[n_robots*3:n_robots*3+n_robots*3])
-    obj = obj+st_error.T@Q@st_error + inp.T@R@inp #calculate obj
+    obj = obj+st_error.T @ Q @ st_error + inp.T @ R @ inp #calculate obj
     st_next = X[:,k+1]
-    f_value = f[st,inp]
+    f_value = f(st,inp) #~~check
     st_next_euler = st+ (T*f_value)
     g.append(st_next-st_next_euler) #compute constraints for equality state constraints
+
+
+
+
+
+
+
+
